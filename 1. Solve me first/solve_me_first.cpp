f9255c31aac7d6b07f2639d4d8609b0d69aea36a
Ayush8165 <thakur.ayush8165@gmail.com>
2023-07-24 21:03:57 +0530

    problem1

---
+++ 1. Solve me first/solve_me_first.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int sum(int a, int b)
+{
+    return a+b;
+}
+
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<sum(a,b); 
+    return 0;
+}
